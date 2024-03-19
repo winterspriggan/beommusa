22a4fd6d4addf8ac19e1c90b9aa4f88b4cbd05de
winterspriggan <jin-_-x@naver.com>
2024-03-19 15:59:24 +0900

    initial setting complete

--- frontend/src/component/Button/Button.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import './Button.css';
-
-export default function Button({text, type = 'submit', onClick}) {
-    return (
-        <button className='Button' type={type} onClick={onClick}>{text}</button>
-    );
-}
